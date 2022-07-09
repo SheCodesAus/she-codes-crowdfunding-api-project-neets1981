becac6d73856b92a0e4d33d1cec645b51b9df335
neets1981 <neeta.patankar@autogeneral.com.au>
2022-07-09 11:56:49 +1000

    Update & Delete user

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework import status, permissions
+from .permission import IsOwnerOrReadOnly
 
 # Create your views here.
 # users/
@@ -25,6 +26,9 @@ class CustomUserList(APIView):
 
 # /users/pk        
 class CustomUserDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly
+        ]
     def get_object(self, pk):
         try:
             return CustomUser.objects.get(pk=pk)
@@ -35,3 +39,11 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+
+    def delete(self, request, pk):
+        user = self.get_object(pk)
+        if user.id == request.user.id:
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            status=status.HTTP_401_UNAUTHORIZED)
