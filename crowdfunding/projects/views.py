3f8748b163e699f8256107ccf512b1a275859c48
neets1981 <neeta.patankar@autogeneral.com.au>
2022-07-09 10:25:58 +1000

    proj commit

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -36,8 +36,7 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(owner=request.user)
-            return Response(serializer.data
-            status=status.HTTP_201_CREATED)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 # projexts/<pk>
